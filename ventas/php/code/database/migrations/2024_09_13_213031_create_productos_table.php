e605377eee154f09a2be54f0e9925dedc5ab5826
Yan Cubides <yanccs@gmail.com>
2024-09-13 19:49:48 -0400

    Se agregaron las tablas de producto, usuarios, facturas y detalle factura

---
+++ ventas/php/code/database/migrations/2024_09_13_213031_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto');
+            $table->string('unidad');
+            $table->decimal('precio', 8, 2);
+            $table->integer('existencia');
+            $table->decimal('impuesto',6,2);
+            $table->enum('estatus', ['Activo', 'Inactivo'])->default('Activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ ventas/php/code/database/migrations/2024_09_13_213504_create_detallefactura_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detallefactura', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('idfactura')->constrained('facturas');
+            $table->foreignId('idproducto')->constrained('productos');
+            $table->string('producto');
+            $table->decimal('precio', 8, 2);
+            $table->integer('cantidad');
+            $table->integer('impuesto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detallefactura');
+    }
+};
